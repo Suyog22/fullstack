93ab7985c38e419f44933b570219b60492175816
Suyog22 <majgaonkar.suyog@gmail.com>
2020-02-13 14:31:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-euynre
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-euynre)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Angular 6';
+  constructor(private http: HttpClient) { }
+  url="http://localhost:8080/listofmobiles";
+
+  mobileName : String;
+  price: String;
+
+  httpData:any;
+  ngOnInit() {
+    this.http.get<Mobile[]>(this.url).subscribe(data => {
+      this.httpData=data;
+    })
+
+  }
+
+
+
+}
+interface Mobile {
+  name:string;
+  price:string;
+}
